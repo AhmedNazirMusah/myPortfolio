b5d219e66633a5fa422809965460219ce2dba05a
Ahmed Nazir Musah <ahmed999nazir@gmail.com>
2022-09-06 13:23:02 +0000

    added the blur background when modal is opened and fixed the position of the modal

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const menu = document.querySelector('.li-class2');
 const detail = document.querySelector('.mobile-modal');
 const buttonX = document.querySelector('.close-btn');
+const body = document.querySelector('.bbb');
 
 function open() {
   detail.classList.toggle('hide');
   menu.classList.toggle('hide');
+  body.classList.toggle('blur');
 }
 
 function close() {
   detail.classList.toggle('hide');
   menu.classList.toggle('hide');
+  body.classList.toggle('blur');
 }
 
 menu.addEventListener('click', open);
